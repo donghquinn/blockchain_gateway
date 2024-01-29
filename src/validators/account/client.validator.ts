3b5247acd32859071c0a11550ce3350a82ed3b41
donghquinn <ehdgus1524@gmail.com>
2024-01-30 08:20:10 +0900

    added: get balance

--- src/validators/account/client.validator.ts
+++ src/validators/account/client.validator.ts
@@ -1,6 +1,6 @@
 import { ValidatorError } from '@errors/validator.error';
 import { Logger } from '@utils/logger.util';
-import { LoginClientRequest, LogoutClientRequest } from 'types/account/client.type';
+import { BalanceClientRequest, LoginClientRequest, LogoutClientRequest } from 'types/account/client.type';
 import { z } from 'zod';
 
 export const loginRequestValidator = async (request: LoginClientRequest) => {
@@ -34,6 +34,24 @@ export const logoutRequestValidator = async (request: LogoutClientRequest) => {
     throw new ValidatorError(
       '[LOGOUT] Validate Logout Account',
       'Validate Logout Account Error. Please Try Again.',
+      error instanceof Error ? error : new Error(JSON.stringify(error)),
+    );
+  }
+};
+
+export const balanceRequestValidator = async (request: BalanceClientRequest) => {
+  try {
+    const scheme = z.object({ uuid: z.string() });
+
+    const validated = await scheme.parseAsync(request);
+
+    return validated;
+  } catch (error) {
+    Logger.error('[LOGIN] Validate Login Account Error: %o', { error });
+
+    throw new ValidatorError(
+      '[ACCOUNT] Validate Login Account',
+      'Validate Login Account Error. Please Try Again.',
       error instanceof Error ? error : new Error(JSON.stringify(error)),
     );
   }
