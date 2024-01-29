dd5f22e0bfdb684962d9b05dd3452c43cd8a9cc5
donghquinn <ehdgus1524@gmail.com>
2024-01-30 08:23:14 +0900

    wip: middleware register

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,8 +1,14 @@
 import { ClientModule } from '@modules/account/account.module';
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { globalMiddleware } from 'middlewares/global.middlewares';
 import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
   imports: [ClientModule, TransactionModule],
 })
-export class AppModule {}
+export class AppModule  implements NestModule {
+  // eslint-disable-next-line class-methods-use-this
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(globalMiddleware).forRoutes('*');
+  }
+}
