9cdcd2b78995900e0077eb14310dfd15d10a56ab
donghquinn <ehdgus1524@gmail.com>
2024-01-30 05:41:55 +0900

    fix: tx module

--- src/modules/transaction/transaction.module.ts
+++ src/modules/transaction/transaction.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { TransactionProvider } from "providers/transaction/transaction.pvd";
 import { TransactionPrismaModule } from "./transaction-prisma.module";
 import { TransactionController } from "@controllers/transaction/transaction.ctl";
+import { Web3Module } from "@modules/web3.module";
 
 @Module({
   providers: [TransactionProvider],
   controllers: [TransactionController],
-  imports: [TransactionPrismaModule],
+  imports: [TransactionPrismaModule, Web3Module],
 })
 export class TransactionModule {}
