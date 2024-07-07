a816f83c362b382ea736ca90c332613d38338d7a
donghquinn <ehdgus1524@gmail.com>
2024-07-08 00:22:14 +0900

    wip: signup

--- src/controllers/users/signup.ctl.ts
+++ src/controllers/users/signup.ctl.ts
@@ -1,8 +1,21 @@
 import { setErrorResponse } from '@dto/index';
+import { SetSignupResponse } from '@dto/users/user.dto';
+import { signupNewUser } from '@libraries/user/signup.lib';
+import { signupValidator } from '@validators/users/signup.validator';
 import { DefaultContext } from 'koa';
+import { UserSignupRequest } from 'types/user.type';
 
-export const signupController = async (ctx: DefaultContext) => {
+export const signupController = async (ctx: UserSignupRequest) => {
   try {
+    const { email, name, password } = await signupValidator(ctx);
+
+    const signupResult = await signupNewUser(email, name, password);
+
+    if (!signupResult) {
+      return setErrorResponse(ctx, '01', new Error('[SIGNUP] Signup Failed'));
+    }
+
+    return SetSignupResponse(ctx, '01');
   } catch (error) {
     return setErrorResponse(ctx, '99', new Error('[SIGNUP] Signup Failed'));
   }
