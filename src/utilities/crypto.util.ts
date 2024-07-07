783a3f1367525fadb3e29e031110e56162affc75
donghquinn <ehdgus1524@gmail.com>
2024-07-07 17:06:36 +0900

    wip: crypto

--- src/utilities/crypto.util.ts
+++ src/utilities/crypto.util.ts
@@ -1,4 +1,4 @@
-import { createCipheriv } from 'crypto';
+import { createCipheriv, createDecipheriv } from 'crypto';
 
 export const encryptString = (arg: string) => {
   const secretKey = process.env.SECRET_KEY!;
@@ -12,4 +12,19 @@ export const encryptString = (arg: string) => {
   const encryptedString = Buffer.concat([encrypted, cipher.final()]).toString('hex');
 
   return encryptedString;
+};
+
+export const decryptString = (arg: string) => {
+  const secretKey = process.env.SECRET_KEY!;
+  const baseIv = process.env.AES_IV!;
+  const iv = Buffer.from(baseIv, 'hex');
+
+  const encText = Buffer.from(arg, 'hex');
+
+  const decipher = createDecipheriv('aes-256-cbc', Buffer.from(secretKey), iv);
+
+  const decrypted = decipher.update(encText);
+  const decryptedString = Buffer.concat([decrypted, decipher.final()]).toString();
+
+  return decryptedString;
 };
